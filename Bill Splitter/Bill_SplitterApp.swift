dd443ced8a7bf7de677998d27c6bf6d522dc5726
rosh324 <sevaliaroshan@gmail.com>
2021-08-11 08:39:27 -0700

    Initial Commit

---
+++ Bill Splitter/Bill_SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bill_SplitterApp.swift
+//  Bill Splitter
+//
+//  Created by Roshan Sevalia on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Bill_SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
